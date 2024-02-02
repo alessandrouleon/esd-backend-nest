2e879173fa824fa29516aee4b0e0c1f70851206a
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2024-02-02 15:30:02 -0400

    feat: ajuste na url do banco

--- prisma/migrations/20240202182649_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Employees" ALTER COLUMN "createdAt" SET DEFAULT NOW();
-
--- AlterTable
-ALTER TABLE "Lines" ALTER COLUMN "createdAt" SET DEFAULT NOW();
-
--- AlterTable
-ALTER TABLE "Users" ALTER COLUMN "createdAt" SET DEFAULT NOW();
---
+++ prisma/migrations/20240202192839_/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE `Lines` (
+    `id` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(24) NOT NULL,
+    `stage` VARCHAR(24) NULL,
+    `description` VARCHAR(256) NULL,
+    `createdAt` DATETIME NOT NULL DEFAULT NOW(),
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Lines_code_key`(`code`),
+    UNIQUE INDEX `Lines_description_key`(`description`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Employees` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `registration` VARCHAR(191) NOT NULL,
+    `department` VARCHAR(191) NOT NULL,
+    `shift` VARCHAR(64) NOT NULL,
+    `linesId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME NOT NULL DEFAULT NOW(),
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Employees_registration_key`(`registration`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(64) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(64) NOT NULL,
+    `permission` VARCHAR(64) NOT NULL,
+    `employeeId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME NOT NULL DEFAULT NOW(),
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Users_userName_key`(`userName`),
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Employees` ADD CONSTRAINT `Employees_linesId_fkey` FOREIGN KEY (`linesId`) REFERENCES `Lines`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Users` ADD CONSTRAINT `Users_employeeId_fkey` FOREIGN KEY (`employeeId`) REFERENCES `Employees`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
